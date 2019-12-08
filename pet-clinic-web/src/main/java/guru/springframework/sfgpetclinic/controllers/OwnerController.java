effefff74c692c22229bf45ab6314a24e3f3db5d
kursatcinar <kursatcinar@gmail.com>
2019-12-09 01:16:58 +0300

    added Vet list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -19,6 +19,7 @@ public class OwnerController {
     public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
